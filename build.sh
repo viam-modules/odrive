8ba42330697927625f431269557d906a236e8bea
Steve Briskin <steve@viam.com>
2026-02-27 15:20:02 -0500

    fix build

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ cd `dirname $0`
 VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 
+if ! $PYTHON -m pip install -r requirements.txt -qq; then
+    exit 1
+fi
+
 if ! $PYTHON -m pip install pyinstaller setuptools -Uqq; then
     exit 1
 fi
@@ -12,6 +16,7 @@ fi
 $PYTHON -m PyInstaller --onefile \
     --hidden-import="googleapiclient" \
     --runtime-hook="runtime_hooks/hook_pkg_resources.py" \
+    --collect-all="viam" \
     --collect-all="odrive" \
     src/main.py
 tar -czvf dist/archive.tar.gz meta.json ./dist/main
