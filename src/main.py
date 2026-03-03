6bef18f3cfe010c76ed132f3438645ba44587bbd
Steve Briskin <steve@viam.com>
2026-03-03 17:06:12 -0500

    remove the can model

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import asyncio
 from viam.module.module import Module
 
 from models.odriveSerial import OdriveSerial
-from models.odriveCAN import OdriveCAN
-
 
 if __name__ == '__main__':
     asyncio.run(Module.run_from_registry())
