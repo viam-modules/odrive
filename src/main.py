edab891787d9109be043b50b674ba81b0465ce32
Steve Briskin <steve@viam.com>
2026-03-06 16:28:41 -0500

    Revert "remove the can model"
    This reverts commit 6bef18f3cfe010c76ed132f3438645ba44587bbd.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import asyncio
 from viam.module.module import Module
 
 from models.odriveSerial import OdriveSerial
+from models.odriveCAN import OdriveCAN
+
 
 if __name__ == '__main__':
     asyncio.run(Module.run_from_registry())
